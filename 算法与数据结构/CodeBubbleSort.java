14ff36335fb736fee622455b33f118d2b58968ee
SongQinshu <280872387@qq.com>
2018-12-01 16:32:19 +0800

    数据结构与算法实现

---
+++ 算法与数据结构/CodeBubbleSort.java
@@ -0,0 +1,33 @@
+public class CodeBubbleSort {
+    public static void bubbleSort(int[] arr){
+        if(arr == null||arr.length<2){
+            return;
+        }
+        for(int end=arr.length-1;end>0;end--) {
+            for (int i = 0; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+
+            }
+        }
+    }
+    public static void swap(int[] arr,int i,int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    /*
+            test测试方法
+     */
+    public static void main(String args[]){
+        int test[] = {1,232,4,432,5,35,1213,3213,532423,};
+        System.out.println(test.length);
+
+        bubbleSort(test);
+        for (int arr: test
+             ) {
+            System.out.println(arr);
+        }
+    }
+}
